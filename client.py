09851d8d2d9d89409593e72612894f7fdaeb7b09
ninjamar <70116773+ninjamar@users.noreply.github.com>
2026-02-26 15:59:12 +0800

    Moved swift bindings to class

--- client.py
+++ client.py
@@ -3,42 +3,55 @@ import cv2
 import os
 import shutil
 
-lib = ctypes.cdll.LoadLibrary("libpgm.dylib")
+class PgmBindings:
+    def __init__(self, path: str) -> None:
+        self.lib = ctypes.cdll.LoadLibrary(path)
 
-lib.run_photogrammetry_session.arg_types = [ctypes.c_char_p]
-lib.run_photogrammetry_session.restype = None
+        self.lib.run_photogrammetry_session.arg_types = [ctypes.c_char_p]
+        self.lib.run_photogrammetry_session.restype = None
 
-lib.is_completed.argtypes = []
-lib.is_completed.restype = ctypes.c_bool
+        self.lib.is_completed.argtypes = []
+        self.lib.is_completed.restype = ctypes.c_bool
 
-lib.get_progress.argtypes = []
-lib.get_progress.restype = ctypes.c_double
+        self.lib.get_progress.argtypes = []
+        self.lib.get_progress.restype = ctypes.c_double
 
-lib.get_eta.argtypes = []
-lib.get_eta.restype = ctypes.c_double
+        self.lib.get_eta.argtypes = []
+        self.lib.get_eta.restype = ctypes.c_double
 
-lib.stop_photogrammetry_session.argtypes = []
-lib.stop_photogrammetry_session.restype = None
+        self.lib.stop_photogrammetry_session.argtypes = []
+        self.lib.stop_photogrammetry_session.restype = None
+    
+    def start(self, path: bytes) -> None:
+        return self.lib.run_photogrammetry_session(path)
+    
+    def stop(self) -> None:
+        return self.lib.stop_photogrammetry_session()
+    
+    def is_completed(self) -> bool:
+        return self.lib.is_completed()
+    
+    def get_progress(self) -> float:
+        return self.lib.get_progress()
+    
+    def get_eta(self) -> float:
+        return self.lib.get_eta()
+    
 
 
-class Photogrammetry():
+class Photogrammetry:
+    def __init__(self, lib: PgmBindings) -> None:
+        self.recording: list[cv2.typing.MatLike] = []
+        self.frame_rate: int = 30
+    
+    def on_new_frame_callback(self, frame: cv2.typing.MatLike) -> None:
+        frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
+        self.recording.append(frame)
 
-    def set_frame_rate(self, _, frame_rate):
-        self.frame_rate = frame_rate
-
-    def start_recording(self):
-        if self.is_writing:
-            return
+    def start_recording(self) -> None:
         self.recording = []
-        self.is_recording = True
 
     def stop_recording(self):
-        if not self.is_recording:
-            return
-        if self.is_writing:
-            return
-        
-        self.is_recording = False
         os.makedirs("pgm", exist_ok=True)
 
         if os.path.exists("pgm/recording"):
@@ -46,33 +59,17 @@ class Photogrammetry():
 
         os.makedirs("pgm/recording", exist_ok=True)
 
-        self.is_writing = True
         video_writer = cv2.VideoWriter("pgm/recording/video.avi", cv2.VideoWriter_fourcc(*"MJPG"), 30, (800, 600))
         for frame in self.recording:
             video_writer.write(frame)
         video_writer.release()
-        self.is_writing = False
     
-    def get_progress(self):
-        return self.lib.get_progress()
-        # dpg.set_value(self.progress_bar, progress)
-        # dpg.configure_item(self.progress_bar, overlay=f"{100 * progress:.2f}%")
-        # dpg.set_value(self.eta_indicator, f"ETA: {eta:.2f}s")
-    def get_eta(self):
-        return self.lib.get_eta()
-
     def start_reconstruction(self):
-        if self.running:
-            return
-        
-        self.running = True
-
         os.makedirs("pgm", exist_ok=True)
         if os.path.exists("pgm/reconstruction"):
             shutil.rmtree("pgm/reconstruction")
         os.makedirs("pgm/reconstruction/model", exist_ok=True)
 
-        self.is_reading = True
 
         video_capture = cv2.VideoCapture("pgm/recording/video.avi")
         # read all frames
@@ -88,8 +85,6 @@ class Photogrammetry():
             
             frame_count += 1
         video_capture.release()
-
-        self.is_reading = False
             
         # frames = len(os.listdir("pgm/recording"))
         # skip_frames = max(1, 30 // self.frame_rate)
@@ -99,28 +94,24 @@ class Photogrammetry():
         self.lib.run_photogrammetry_session(b"pgm/reconstruction")
         
     def stop_reconstruction(self):
-        if not self.running:
-            return
-        self.running = False
         self.lib.stop_photogrammetry_session()
+    
+    def set_frame_rate(self, _, frame_rate):
+        self.frame_rate = frame_rate
 
+    def get_progress(self):
+        return self.lib.get_progress()
+        # dpg.set_value(self.progress_bar, progress)
+        # dpg.configure_item(self.progress_bar, overlay=f"{100 * progress:.2f}%")
+        # dpg.set_value(self.eta_indicator, f"ETA: {eta:.2f}s")
+    def get_eta(self):
+        return self.lib.get_eta()
+    
 
-    def __init__(self):
-        self.is_recording = False
-        self.is_writing = False
-        self.is_reading = False
-        self.recording = []
-        self.running = False
-        self.frame_rate = 30
-          
+if __name__ == "__main__":
+    bindings = PgmBindings("libpgm.dylib")
+    photogrammetry = Photogrammetry(bindings)
+    # camera(callback=on_new_frame_callback)
 
-        # label="Open in Preview", callback=lambda: os.system("open pgm/reconstruction/out.usdz")
-        # label="Open in Meshlab", callback=lambda: os.system("open -a /Applications/MeshLab2023.12.app pgm/reconstruction/model/out.obj")
-
-    def on_new_frame_callback(self, frame):
-        if self.is_recording:
-            frame = cv2.cvtColor(frame, cv2.COLOR_RGBA2BGR)
-            self.recording.append(frame)
-
-photogrammetry = Photogrammetry()
-# camera(callback=on_new_frame_callback)
+    # label="Open in Preview", callback=lambda: os.system("open pgm/reconstruction/out.usdz")
+    # label="Open in Meshlab", callback=lambda: os.system("open -a /Applications/MeshLab2023.12.app pgm/reconstruction/model/out.obj")
