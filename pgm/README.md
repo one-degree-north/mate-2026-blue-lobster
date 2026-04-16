e7c82ba948b5034c4d2ccf723f509b5b4bb871a8
ninjamar <70116773+ninjamar@users.noreply.github.com>
2026-04-16 20:36:16 +0800

    Make photogrammetry a module

---
+++ pgm/README.md
@@ -0,0 +1,4 @@
+# RealityKIT
+
+
+Implement Photogrammetery. Takes a video stream and converts it into a 3d model. Need to be able to scale model. Aftering capturing reference object, find length of unknowns.
---
+++ pgm/hatch_build.py
@@ -0,0 +1,19 @@
+import subprocess
+import sys
+from pathlib import Path
+
+from hatchling.builders.hooks.plugin.interface import BuildHookInterface
+
+
+class CustomBuildHook(BuildHookInterface):
+    def initialize(self, version, build_data):
+        pgm_dir = Path(__file__).parent
+        result = subprocess.run(
+            ["just", "build"],
+            #["swiftc", "-emit-library", "-o", str(pgm_dir / "libPgm.dylib"), str(pgm_dir / "pgm.swift")],
+            capture_output=True,
+            text=True,
+        )
+        if result.returncode != 0:
+            print(result.stderr, file=sys.stderr)
+            raise RuntimeError("Swift build failed")
